239d59345f70e2d76c3c52c5ce8937c489e9d248
tepin <tepin.study@gmail.com>
2020-04-17 13:36:00 +0000

    tasklist finish

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
 
-<!-- ここにページ毎のコンテンツを書く -->
+    <h1>タスク一覧</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{!! link_to_route('tasks.show', $task->content, ['id' => $task->id]) !!}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+
+    {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
 
 @endsection
